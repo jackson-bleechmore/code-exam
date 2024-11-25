b8786989853466b565985d15b8b9b2776de0f515
Jackson Bleechmore <jackson.bleechmore@gmail.com>
2024-11-25 17:42:48 +0000

    updated Readme, wrapped twoSumTests in describe statements

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# Welcome to your CDK TypeScript project
+## Take home exam
 
-This is a blank project for CDK development with TypeScript.
+The three questions are answered with unit tests and deployed as lambdas behind a single API gateway.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The project was initialised using:
 
-## Useful commands
+> cdk init app --language typescript
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+## Test:
+
+npm run test
+
+## Deploy:
+
+npm run deploy
+
+```mermaid
+graph TD
+A[<img src="./Arch_Amazon-API-Gateway_32.svg"/>]
+
+B[<img src="./Arch_AWS-Lambda_32.svg">]
+C[<img src="./Arch_AWS-Lambda_32.svg">]
+D[<img src="./Arch_AWS-Lambda_32.svg">]
+
+A -->|GET /two-sum-two| B
+A -->|GET /move-zeros-to-end| C
+A -->|POST /hide-credit-card-number| D
+```
--- lib/lambda/twoSumTwo/twoSumTwo.test.ts
+++ lib/lambda/twoSumTwo/twoSumTwo.test.ts
@@ -1,81 +1,96 @@
-import { twoSumTwo } from './twoSumTwo'
+import { twoSumTwo } from "./twoSumTwo";
 
-test('undefined input array', () => {
+describe("Invalid and empty input", () => {
+  test("undefined input array", () => {
     // @ts-expect-error
-    expect(() => twoSumTwo(undefined, 1)).toThrow()
-})
+    expect(() => twoSumTwo(undefined, 1)).toThrow();
+  });
 
-test('undefined input target', () => {
+  test("undefined input target", () => {
     // @ts-expect-error
-    expect(twoSumTwo([1, 2, 3, 4], undefined)).toEqual([])
-})
+    expect(twoSumTwo([1, 2, 3, 4], undefined)).toEqual([]);
+  });
 
-test('empy array input', () => {
-    expect(twoSumTwo([], 1)).toEqual([])
-})
+  test("empy array input", () => {
+    expect(twoSumTwo([], 1)).toEqual([]);
+  });
+});
 
-test('1 element equal to target', () => {
-    expect(twoSumTwo([10], 10)).toEqual([])
-})
+describe("1 Element ", () => {
+  test("1 element equal to target", () => {
+    expect(twoSumTwo([10], 10)).toEqual([]);
+  });
 
-test('1 element not equal to target', () => {
-    expect(twoSumTwo([10], 20)).toEqual([])
-})
+  test("1 element not equal to target", () => {
+    expect(twoSumTwo([10], 20)).toEqual([]);
+  });
+});
 
-test('2 elements, sum to target', () => {
-    expect(twoSumTwo([10, 20], 30)).toEqual([0, 1])
-})
+describe("2 Elements ", () => {
+  test("Sum to target", () => {
+    expect(twoSumTwo([10, 20], 30)).toEqual([0, 1]);
+  });
 
-test('2 elements, not sum to target', () => {
-    expect(twoSumTwo([10, 20], 50)).toEqual([])
-})
+  test("Not sum to target", () => {
+    expect(twoSumTwo([10, 20], 50)).toEqual([]);
+  });
 
-test('2 elements both 0, target 0', () => {
-    expect(twoSumTwo([0, 0], 0)).toEqual([0, 1])
-})
+  test("Both 0, target 0", () => {
+    expect(twoSumTwo([0, 0], 0)).toEqual([0, 1]);
+  });
 
-test('2 elements both 0, no solution', () => {
-    expect(twoSumTwo([0, 0], 20)).toEqual([])
-})
+  test("Both 0, no solution", () => {
+    expect(twoSumTwo([0, 0], 20)).toEqual([]);
+  });
+});
 
-test('3 elements - first 2 indexes', () => {
-    expect(twoSumTwo([10, 20, 30], 30)).toEqual([0, 1])
-})
+describe("3 Elements ", () => {
+  test("First 2 indexes sum to target", () => {
+    expect(twoSumTwo([10, 20, 30], 30)).toEqual([0, 1]);
+  });
 
-test('3 elements - last 2 indexes', () => {
-    expect(twoSumTwo([10, 20, 30], 50)).toEqual([1, 2])
-})
+  test("Last 2 indexes sum to target", () => {
+    expect(twoSumTwo([10, 20, 30], 50)).toEqual([1, 2]);
+  });
 
-test('3 elements - first and last indexes', () => {
-    expect(twoSumTwo([10, 20, 30], 40)).toEqual([0, 2])
-})
+  test("First and last indexes sum to target", () => {
+    expect(twoSumTwo([10, 20, 30], 40)).toEqual([0, 2]);
+  });
 
-test('4 elements - middle 2 indexes', () => {
-    expect(twoSumTwo([10, 20, 30, 50], 50)).toEqual([1, 2])
-})
+  test("No solution", () => {
+    expect(twoSumTwo([10, 20, 30], 0)).toEqual([]);
+  });
+});
 
-test('4 elements - multiple solutions', () => {
-    expect(twoSumTwo([10, 20, 30, 40], 50)).toEqual([1, 2])
-})
+describe("4 Elements ", () => {
+  test("Middle 2 indexes sum to target", () => {
+    expect(twoSumTwo([10, 20, 30, 50], 50)).toEqual([1, 2]);
+  });
 
-test('All negative values', () => {
-    expect(twoSumTwo([-10, -20, -30, -40], -70)).toEqual([2, 3])
-})
+  test("Multiple solutions", () => {
+    expect(twoSumTwo([10, 20, 30, 40], 50)).toEqual([1, 2]);
+  });
 
-test('Negative and positive values', () => {
-    expect(twoSumTwo([-10, 10, -30, 40], 0)).toEqual([0, 1])
-})
+  test("All negative values", () => {
+    expect(twoSumTwo([-10, -20, -30, -40], -70)).toEqual([2, 3]);
+  });
+});
 
-test('Large input array 50 elements', () => {
+describe("Other inputs", () => {
+  test("Negative and positive values", () => {
+    expect(twoSumTwo([-10, 10, -30, 40], 0)).toEqual([0, 1]);
+  });
+
+  test("Large input array 50 elements", () => {
     const nums = [
-        -66, 56, -71, -2, 19, 67, 17, 86, -44, -63, -50, -65, -33, 2, 14, 18,
-        -74, -81, 31, -31, -90, -21, -61, 57, 39, 11, -96, -63, 93, -14, 43,
-        -98, 16, 91, 1, -44, -77, 76, 89, -70, 10, 32, 24, 57, -3, 42, 89, 21,
-        -18, 74,
-    ]
+      -66, 56, -71, -2, 19, 67, 17, 86, -44, -63, -50, -65, -33, 2, 14, 18, -74,
+      -81, 31, -31, -90, -21, -61, 57, 39, 11, -96, -63, 93, -14, 43, -98, 16,
+      91, 1, -44, -77, 76, 89, -70, 10, 32, 24, 57, -3, 42, 89, 21, -18, 74,
+    ];
 
-    const i = 25
-    const j = 36
+    const i = 25;
+    const j = 36;
 
-    expect(twoSumTwo(nums, nums[i] + nums[j])).toEqual([i, j])
-})
+    expect(twoSumTwo(nums, nums[i] + nums[j])).toEqual([i, j]);
+  });
+});
